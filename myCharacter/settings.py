4ad37447ff7170ec8993803408b4c5de4c4d2ce2
Tyler Davis <tylerrusselldavis@gmail.com>
2021-10-27 15:33:10 -0500

    Added sidebar to main template

--- myCharacter/settings.py
+++ myCharacter/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'crispy_forms',
     'users',
     'characters',
 ]
@@ -132,6 +133,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
