74fb0ee7902b46da9213cd964fe36c3bda5a3bf6
Tyler Davis <tylerrusselldavis@gmail.com>
2021-11-30 15:48:42 -0600

    Made login, registration and home page look a bit nicer

--- users/forms.py
+++ users/forms.py
@@ -2,9 +2,14 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
+    helper = FormHelper()
+    helper.add_input(Submit('submit', 'Submit', css_class='btn-primary'))
+    helper.form_method = 'POST'
 
     class Meta:
         model = User
